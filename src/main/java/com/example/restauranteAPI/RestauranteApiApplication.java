0463bf70a2334116a46b7cd1b3f709c630d0cf3b
Antony <antony.montilha@hotmail.com>
2025-04-05 20:43:24 -0300

    config cors

--- src/main/java/com/example/restauranteAPI/RestauranteApiApplication.java
+++ src/main/java/com/example/restauranteAPI/RestauranteApiApplication.java
@@ -2,12 +2,22 @@ package com.example.restauranteAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class RestauranteApiApplication {
+public class RestauranteApiApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestauranteApiApplication.class, args);
 	}
-
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**") // Aplica a todas as rotas
+				.allowedOrigins("https://d2wk73bhxwtu9d.cloudfront.net") // Seu domínio CloudFront
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowedHeaders("*")
+				.allowCredentials(true)
+				.maxAge(3600); 
+	}
 }
